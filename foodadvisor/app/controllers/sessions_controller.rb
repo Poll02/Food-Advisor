5fde60353f998546aaae1f4fc9a7007f0e70a129
Leonardo Vona <vonaleonardo20@gmail.com>
2024-06-23 17:39:28 +0200

    Modificata la pagina Classifiche, implementata la funzionalità di Login, implementati messaggi di errore

--- foodadvisor/app/controllers/sessions_controller.rb
+++ foodadvisor/app/controllers/sessions_controller.rb
@@ -1,4 +1,21 @@
+# app/controllers/sessions_controller.rb
 class SessionsController < ApplicationController
   def new
+  end
+
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      log_in(user)
+      redirect_to root_path, notice: "Login effettuato con successo!"
+    else
+      flash.now[:alert] = 'Combinazione email/password non valida'
+      render 'new'
+    end
+  end
+
+  def destroy
+    log_out
+    redirect_to root_url
   end
 end
