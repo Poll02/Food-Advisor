277da139f6d1f92a4343f782292de03f8cb7228d
marino1984826 <marino1984826@studenti.uniroma1.it>
2024-06-24 12:01:49 +0200

    user stories per il cambio globale delle impostazioni

--- foodadvisor/app/controllers/application_controller.rb
+++ foodadvisor/app/controllers/application_controller.rb
@@ -1,4 +1,10 @@
 class ApplicationController < ActionController::Base
-    include SessionsHelper
+  include SessionsHelper
+  before_action :set_global_settings
 
+  private
+
+  def set_global_settings
+    @setting = Setting.first_or_initialize
+  end
 end
