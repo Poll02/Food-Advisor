eb504ddc3eca490a364664139699194f07e25ada
marino1984826 <marino1984826@studenti.uniroma1.it>
2024-06-25 19:09:05 +0200

    aggiustato login

--- foodadvisor/app/controllers/sessions_controller.rb
+++ foodadvisor/app/controllers/sessions_controller.rb
@@ -6,18 +6,29 @@ class SessionsController < ApplicationController
     end
   end
 
-  def create
+  def create_user
     if request.env['omniauth.auth'] # Login tramite Google
       handle_google_login
     else # Login normale
-      if params[:user]
-        handle_user_login
-      elsif params[:restaurateur]
-        handle_restaurateur_login
+      user = User.find_by(email: params[:session][:email].downcase)
+      if user && user.authenticate(params[:session][:password])
+        log_in(user, 'User')  # Metodo per effettuare il login definito altrove
+        redirect_to root_path
       else
-        flash.now[:alert] = 'Tipo di login non valido.'
-        render 'new'
+        flash.now[:alert] = 'Combinazione email/password non valida per l\'utente.'
+        render 'new_user'  # Renderizza nuovamente il form di login per l'utente
       end
+    end
+  end
+
+  def create_restaurateur
+    restaurateur = Ristoratori.find_by(piva: params[:restaurateur][:piva])
+    if restaurateur && restaurateur.authenticate(params[:restaurateur][:password])
+      log_in(restaurateur, 'Ristoratore')  # Metodo per effettuare il login per il ristoratore
+      redirect_to root_path
+    else
+      flash.now[:alert] = 'Combinazione P.IVA/password non valida per il ristoratore.'
+      render 'new_restaurateur'  # Renderizza nuovamente il form di login per il ristoratore
     end
   end
   
@@ -35,25 +46,4 @@ class SessionsController < ApplicationController
     redirect_to root_path
   end
 
-  def handle_user_login
-    user = User.find_by(email: params[:user][:email].downcase)
-    if user && user.authenticate(params[:user][:password])
-      log_in(user, 'User') # Imposta il ruolo come 'User'
-      redirect_to root_path
-    else
-      flash.now[:alert] = 'Combinazione email/password non valida per l\'utente.'
-      render 'new'
-    end
-  end
-  
-  def handle_restaurateur_login
-    restaurateur = Ristoratori.find_by(piva: params[:restaurateur][:piva])
-    if restaurateur && restaurateur.authenticate(params[:restaurateur][:password])
-      log_in(restaurateur, 'Ristoratore') # Imposta il ruolo come 'Ristoratore'
-      redirect_to root_path
-    else
-      flash.now[:alert] = 'Combinazione P.IVA/password non valida per il ristoratore.'
-      render 'new'
-    end
-  end
 end
--- foodadvisor/app/models/ristoratori.rb
+++ foodadvisor/app/models/ristoratori.rb
@@ -8,5 +8,11 @@ class Ristoratori < ApplicationRecord
     validates :email, presence: true, uniqueness: true
     validates :phone, presence: true
     validates :password, presence: true, length: { minimum: 6 }
+
+    # Metodo authenticate
+    def authenticate(password)
+      # implementazione di base di has_secure_password
+      return self if BCrypt::Password.new(password_digest) == password
+    end
   end
   
