3c84aa649f79f8b9496075adbed478d0b0f562b9
Puls02 <andrea.puls17@gmail.com>
2024-07-07 01:33:01 +0200

    rimosso debug

--- foodadvisor/app/controllers/segnalaziones_controller.rb
+++ foodadvisor/app/controllers/segnalaziones_controller.rb
@@ -6,14 +6,10 @@ class SegnalazionesController < ApplicationController
       )
       @segnalazione.recensione = @recensione
       @segnalazione.cliente = @current_user.cliente
-      Rails.logger.info "Contenuto dell'oggetto @segnalazione prima del salvataggio:"
-        Rails.logger.info @segnalazione.inspect
       if @segnalazione.save
-        Rails.logger.info "YEEEEEEEEEEEEEEEEEEEEEEEE"
         flash[:notice] = 'Segnalazione salvata con successo!'
         redirect_to public_restaurant_profile_path(@recensione.ristoratore_id)
       else
-        Rails.logger.info "NOOOOOOOOOOOOOOOOOOOOOO"
         flash[:alert] = 'Errore durante il salvataggio della recensione'
         redirect_back(fallback_location: root_path)
       end
