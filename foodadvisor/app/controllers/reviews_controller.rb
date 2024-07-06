0b91b0f1fd4b824f77dd56fc8e71c80b505c6ee2
Puls02 <andrea.puls17@gmail.com>
2024-07-07 01:27:26 +0200

    merge con andrea per aggiunta di segnalazioni, vanno sistemati i css del profilo dell'admin

--- foodadvisor/app/controllers/reviews_controller.rb
+++ foodadvisor/app/controllers/reviews_controller.rb
@@ -1,26 +1,27 @@
 # app/controllers/reviews_controller.rb
 class ReviewsController < ApplicationController
-    before_action :set_restaurant_owner
-    def create
-        @review = Recensione.new(
-          stelle: params[:stelle],
-          commento: params[:commento]
-        )
-        @review.cliente = @current_user.cliente
-        @review.ristoratore = @restaurant_owner.ristoratore
-        if @review.save
-          flash[:notice] = 'Recensione salvata con successo!'
-          redirect_to public_restaurant_profile_path(@review.ristoratore_id)
-        else
-          flash[:alert] = 'Errore durante il salvataggio della recensione'
-          redirect_back(fallback_location: root_path)
-        end
-    end
-  
-    private
-  
-    def review_params
-        params.permit(:cliente_id, :ristoratore_id, :stelle, :commento) 
-    end
+  before_action :set_restaurant_owner
+  def create
+      @review = Recensione.new(
+        stelle: params[:stelle],
+        commento: params[:commento]
+      )
+      @review.cliente = @current_user.cliente
+      @review.ristoratore = @restaurant_owner.ristoratore
+      if @review.save
+        flash[:notice] = 'Recensione salvata con successo!'
+        redirect_to public_restaurant_profile_path(@review.ristoratore_id)
+      else
+        flash[:alert] = 'Errore durante il salvataggio della recensione'
+        redirect_back(fallback_location: root_path)
+      end
   end
-  
+
+  private
+
+
+  def set_restaurant_owner
+    @restaurant_owner = Cliente.find(params[:ristoratore_id])
+  end
+
+end
--- foodadvisor/db/schema.rb
+++ foodadvisor/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2024_07_06_110750) do
+ActiveRecord::Schema.define(version: 2024_07_06_204856) do
 
   create_table "admins", force: :cascade do |t|
     t.integer "utente_id", null: false
