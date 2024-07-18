b9a259814218e4611fa4edc54752aed183bae0b0
GiorgioTuk <giorgio.tuccinardi8@gmail.com>
2024-07-18 17:55:07 +0200

    fix controller recensioni, fix home_controller, fix home_controller

--- foodadvisor/app/controllers/home_controller.rb
+++ foodadvisor/app/controllers/home_controller.rb
@@ -1,7 +1,7 @@
 class HomeController < ApplicationController
   def index
     @recent_restaurants = Ristoratore.includes(cliente: :utente).where("ristoratores.created_at >= ?", 30.days.ago).order(created_at: :desc).limit(10)
-    @upcoming_events = Evento.where("data >= ?", Date.today).order(data: :asc)
+    @upcoming_events = Evento.where("data >= ?", Date.today).order(data: :asc).limit(10)
     @reviews = Recensione.top_rated
   end
 end
